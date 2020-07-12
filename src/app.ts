9a1c979173525d9a02f5d185f6a6410b15875f5f
lmt <1571min@naver.com>
2020-07-12 22:20:34 +0900

    ADD: add App Class

---
+++ src/app.ts
@@ -0,0 +1,23 @@
+import express from 'express';
+class App {
+  public app: express.Application;
+  public port: number;
+
+  constructor(port: number) {
+    this.app = express();
+    this.port = port;
+    this.initializeMiddlewares();
+  }
+
+  private initializeMiddlewares() {
+    console.log('this is middleware');
+  }
+
+  public listen() {
+    this.app.listen(this.port, () => {
+      console.log(`App listening on the port ${this.port}`);
+    });
+  }
+}
+
+export default App;
