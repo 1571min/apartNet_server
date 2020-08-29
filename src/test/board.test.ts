03101e36ce41eff6f12da7d3bb015c3bf0219c1c
1571min <1571min@naver.com>
2020-08-29 10:59:46 +0900

    ADD: boardtest init

---
+++ src/test/board.test.ts
@@ -0,0 +1,47 @@
+import request from 'supertest';
+import App from '../app';
+import { getRepository, createConnection, getConnectionOptions } from 'typeorm';
+
+const config = require('../../../ormconfig');
+
+const app = new App(5000);
+const agent = request(app.app);
+
+describe('User Test', () => {
+  beforeAll(async () => {
+    const option = await getConnectionOptions();
+    Object.assign(option, config);
+    const connection = await createConnection(option);
+  });
+  afterAll(async () => {});
+  describe('POST /board', () => {
+    it('should return 200 OK', () => {
+      return agent
+        .post('/board')
+        .send({
+          name: 'TestName',
+          apartmentId: 1,
+          content: 'this is Test content',
+          tag: ['apart', 'tag'],
+          address_name: 'test address name',
+          address: 'test address',
+        })
+        .expect(200);
+    });
+  });
+  describe('GET /board', () => {
+    it('should return 200 OK', async () => {
+      const res = await agent.get('/board');
+      return expect(res.body).toEqual([
+        {
+          name: 'TestName',
+          apartmentId: 1,
+          content: 'this is Test content',
+          tag: ['apart', 'tag'],
+          address_name: 'test address name',
+          address: 'test address',
+        },
+      ]);
+    });
+  });
+});
